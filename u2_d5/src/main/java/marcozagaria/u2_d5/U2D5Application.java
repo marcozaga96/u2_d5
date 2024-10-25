07594fc1960eed0e12a26deb0e50503f104fd852
Marco Zagaria <marcozagaria@live.com>
2024-10-25 10:46:37 +0200

    creazione progetto e collegamento al db

---
+++ u2_d5/src/main/java/marcozagaria/u2_d5/U2D5Application.java
@@ -0,0 +1,13 @@
+package marcozagaria.u2_d5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2D5Application.class, args);
+	}
+
+}
---
+++ u2_d5/src/test/java/marcozagaria/u2_d5/U2D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package marcozagaria.u2_d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
